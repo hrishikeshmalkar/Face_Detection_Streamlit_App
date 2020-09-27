f217b348af14e3190914cfa96ee0f24c56544386
HRISHIKESH <hrishikeshmalkar123@gmail.com>
2020-09-27 17:36:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 
 img=Image.open('logo.png')
-st.beta_set_page_config(page_tile="Face_Detection_App",page_icon=img)
+st.beta_set_page_config(page_title="Face_Detection_App",page_icon=img)
 
 @st.cache
 def load_image(img):
